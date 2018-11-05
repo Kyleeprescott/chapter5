6b3b00670c4c51b1bda23172cd53150805b8c8ef
kylee.prescott <kylee.prescott@CLC-LAB209-09.TCSD.tooelesd.org>
2018-11-05 13:58:43 -0700

    work

---
+++ src/WorkFolder/ConvertLetterGrade.java
@@ -0,0 +1,25 @@
+package WorkFolder;
+import java.util.Scanner;
+public class ConvertLetterGrade {
+
+	public static void main(String[] args) {
+		String Letter;
+		String A;
+		String B;
+		String C;
+		String D;
+		String F;
+		Scanner input = new Scanner(System.in);
+		
+		// prompt the user 
+		System.out.println(" Eneter your letter grade you want to convert");
+	      Letter = input.nextLine();
+	      if (Letter == A ) {
+	    	  
+	      }
+	 
+		
+
+	}
+
+}
