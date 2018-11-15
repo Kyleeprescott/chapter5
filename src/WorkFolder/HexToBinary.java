b24b44bed7e66f317ad2e5585ac85ef5863d7bc4
kylee.prescott <kylee.prescott@CLC-LAB209-09.TCSD.tooelesd.org>
2018-11-15 14:15:02 -0700

    work

--- src/WorkFolder/HexToBinary.java
+++ src/WorkFolder/HexToBinary.java
@@ -4,152 +4,44 @@ public class HexToBinary {
 
 	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);
-		
-		// user input
 		System.out.println("Enter a hex digit");
+		String num1 = input.nextLine();
+		if( num1.equals(0)) {
+			System.out.println("0000");
+		}
+		else if (num1.equals(1))	{
+			System.out.println("0001");
+		}
+		else if (num1.equals(2))	{
+			System.out.println("0010");
+		}else if (num1.equals(3))	{
+			System.out.println("0011");
+		}else if (num1.equals(4))	{
+			System.out.println("0100");
+		}else if (num1.equals(5))	{
+			System.out.println("0101");
+		}else if (num1.equals(6))	{
+			System.out.println("0110");
+		}else if (num1.equals(7))	{
+			System.out.println("0111");
+		}else if (num1.equals(8))	{
+			System.out.println("1000");
+		}else if (num1.equals(9))	{
+			System.out.println("1001");
+		}else if (num1.equals('a'))	{
+			System.out.println("1010");
+		}else if (num1.equals('b'))	{
+			System.out.println("1011");
+		}else if (num1.equals('c'))	{
+			System.out.println("1100");
+		}else if (num1.equals('d'))	{
+			System.out.println("1101");
+		}else if (num1.equals('e'))	{
+			System.out.println("1110");
+		}else if (num1.equals('f'))	{
+			System.out.println("1111");
+		}
 		
-		final int num128 = 128;
-		 final int num64 = 64;
-		 final int num32 = 32;
-		 final int num16 = 16;
-		 final int num8 = 8;
-		 final int num4 = 4;
-		 final int num2 = 2;
-		 final int num1 = 1;
-		 //
-		 String set1="";
-		 String set2="";
-		 String set3="";
-		 String set4="";
-		 String set5="";
-		 String mset1="";
-		 String mset2="";
-		 String mset3="";
-		 String mset4="";
-		 
-		 //input numbers
-		 int inputnum=input.nextLine();
-		 
-		 int whatsLeft;
-		 
-		 
-		 for(int i=0; i<31;i++) {
-			 String biannum ="";
-			 
-			 inputnum = inputnum+1;
-			 
-			 
-			 
-			 whatsLeft=inputnum;
-			 
-			 if(inputnum >+num128) {
-				 whatsLeft= inputnum -num128;
-				 biannum = biannum+"1";
-			 }
-			 else {
-				 biannum = biannum+"0";
-			 }
-			 if(whatsLeft>=num64) {
-				 whatsLeft=  whatsLeft-64;
-				 biannum=biannum+"1";
-				 
-			 }
-			 else {
-				 biannum=biannum+"0";
-			 }
-			 if(whatsLeft>=num32) {
-				 whatsLeft=  whatsLeft-32;
-				 biannum=biannum+"1";
-				 
-			 }
-			 else {
-				 biannum=biannum+"0";
-			 }
-			 if(whatsLeft>=num16) {
-				 whatsLeft=  whatsLeft-16;
-				 biannum=biannum+"1";
-				 
-			 }
-			 else {
-				 biannum=biannum+"0";
-			 }
-			 if(whatsLeft>=num8) {
-				 whatsLeft=  whatsLeft-8;
-				 biannum=biannum+"1";
-				 
-			 }
-			 else {
-				 biannum=biannum+"0";
-			 }
-			 if(whatsLeft>=num4) {
-				 whatsLeft= whatsLeft-4;
-				 biannum=biannum+"1";
-				 
-			 }
-			 else {
-				 biannum=biannum+"0";
-			 }
-			 if(whatsLeft>=num2) {
-				 whatsLeft=  whatsLeft-2;
-				 biannum=biannum+"1";
-				 
-			 }
-			 else {
-				 biannum=biannum+"0";
-			 }
-			 if(whatsLeft>=num1) {
-				 whatsLeft=  whatsLeft-1;
-				 biannum=biannum+"1";
-				 
-			 }
-			 else {
-				 biannum=biannum+"0";
-			 }
-			 
-			 //check indexing of bianary numbers
-			 char xnum7 = biannum.charAt(7);
-			 char xnum6 = biannum.charAt(6);
-			 char xnum5 = biannum.charAt(5);
-			 char xnum4 = biannum.charAt(4);
-			 char xnum3 = biannum.charAt(3);
-			 char mnum7 = biannum.charAt(7);
-			 char mnum6 = biannum.charAt(6);
-			 char mnum5 = biannum.charAt(5);
-			 char mnum4 = biannum.charAt(4);
-			 //assign to sets
-			 if(xnum7=='1') {
-				 set1=set1+inputnum+" ";
-			 }
-			 if(xnum6=='1') {
-				 set2=set2+inputnum+" ";
-			 }
-			 if(xnum5=='1') {
-				 set3=set3+inputnum+" ";
-			 }
-			 if(xnum4=='1') {
-				 set4=set4+inputnum+" ";
-			 }
-			 if(xnum3=='1') {
-				 set5=set5+inputnum+" ";
-			 }
-			 if(inputnum <=12) {
-				 if(mnum7=='1') {
-					 mset1=mset1+inputnum+" ";
-				 }
-				 if(mnum6=='1') {
-					 mset2=mset2+inputnum+" ";
-				 }
-				 if(mnum5=='1') {
-					 mset3=mset3+inputnum+" ";
-				 }
-				 if(mnum4=='1') {
-					 mset4=mset4+inputnum+" ";
-				 }
-			 }
-			 
-		 }
-		 
-
 	}
 
 }
--- src/WorkFolder/VowelOrConsonant.java
+++ src/WorkFolder/VowelOrConsonant.java
@@ -5,11 +5,20 @@ public class VowelOrConsonant {
 	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);
 		//user input
-		System.out.println("Eneter a letter in the alphabet");
-	 String letter = input.nextLine();
-	
-	 if (letter.equals('letter'))
-	
+		System.out.println("Enter a letter");
+		
+		char letter = input.nextLine().charAt(0);
+		
+		
+		if(letter == 'A'|| letter =='E'||letter =='I'|| letter =='O'||letter =='U'||letter =='Y'
+				||letter == 'a'|| letter == 'e'|| letter =='i'||letter=='o'||letter =='u'||letter=='y') {
+			System.out.println(letter+ " is a vowel");
+		}
+		else {
+			
+
+	System.out.println(letter+ " is a consonant");
+		}
 	
 	
 	
