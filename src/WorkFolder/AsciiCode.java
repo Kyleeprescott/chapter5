2ba748be6f6145faf7e869a0382b0e2f25a062ec
kylee.prescott <kylee.prescott@CLC-LAB209-09.TCSD.tooelesd.org>
2018-11-19 13:55:39 -0700

    work

---
+++ src/WorkFolder/AsciiCode.java
@@ -0,0 +1,10 @@
+package WorkFolder;
+
+public class AsciiCode {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/WorkFolder/Unicode.java
@@ -0,0 +1,10 @@
+package WorkFolder;
+import java.util.Scanner;
+public class Unicode {
+
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		
+	}
+
+}
