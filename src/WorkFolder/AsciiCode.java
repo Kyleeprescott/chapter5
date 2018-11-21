4ef94c4038be794b92d2a6928c48a846ddc47730
kylee.prescott <kylee.prescott@CLC-LAB209-09.TCSD.tooelesd.org>
2018-11-21 10:58:42 -0700

    work

--- src/WorkFolder/AsciiCode.java
+++ src/WorkFolder/AsciiCode.java
@@ -1,10 +1,15 @@
 package WorkFolder;
-
+import java.util.Scanner;
 public class AsciiCode {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner input= new Scanner(System.in);
+	System.out.println("Eneter a ASCII code");
+	int asciiNum= input.nextInt();
+	char X = (char)asciiNum;
+	System.out.println(X);
+	
 	}
+	
 
 }
--- src/WorkFolder/Unicode.java
+++ src/WorkFolder/Unicode.java
@@ -4,6 +4,10 @@ public class Unicode {
 
 	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);
+		System.out.println("Enter a letter ");
+		char Q = input.next().charAt(0);
+		int Y = (int)Q;
+		System.out.println(Y);
 		
 	}
 
